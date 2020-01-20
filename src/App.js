c435136f00eeb06adfde02b572579c1b3f1ed78b
Ryan Wolf <ryanwolfsr@gmail.com>
2020-01-20 17:05:12 -0500

    Changed paragraph text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> to change.
+          Change to show feature branch.
         </p>
         <a
           className="App-link"
